f6fd46a8631e2104d3d05c45de7c61678f6054ae
MoazzamAliSE <moazzamali6040@gmail.com>
2022-03-23 14:24:45 +0500

    Day 2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -9,15 +11,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // int days = 30;
+    // double pi = 3.14; // it can be change because it is not final or const
+    // String name = "Moazzam";
+    // bool isMale = true;
+    // num anyDecimalValue = 30.5; // any numerical value
+    // var day = "tuesday"; // var can be any value it can be string or num
+    // const piVakue = 3.14; //let the value that will never change
+    // final list = [
+    //   2,
+    //   4
+    // ]; // final is modifiable means a list can be modifiable after a while
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text(
-                'Welcome Man you are going to complete flutter before 28 March'),
-          ),
-        ),
-      ),
+      home: Homepage(),
     );
   }
 }
