c3834fab0398224f1436fb40e4504bc6a34a14b5
MoazzamAliSE <moazzamali6040@gmail.com>
2022-03-27 11:56:15 +0500

    Day 17 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,19 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+import 'dart:convert';
+import 'package:velocity_x/velocity_x.dart';
 import 'package:flutter_application_1/models/catalog.dart';
 import '../models/catalog.dart';
-import '../widgets/drawer.dart';
-import '../widgets/items_widget.dart';
+import '../widgets/home_widgets/Catalog_Header.dart';
+import '../widgets/home_widgets/Catalog_list.dart';
+import '../widgets/themes.dart';
 
 class Homepage extends StatefulWidget {
-  const Homepage({Key? key}) : super(key: key);
+  // const Homepage({Key? key}) : super(key: key);
 
   @override
-  State<Homepage> createState() => _HomepageState();
+  _HomepageState createState() => _HomepageState();
 }
 
 class _HomepageState extends State<Homepage> {
@@ -39,110 +41,139 @@ class _HomepageState extends State<Homepage> {
 
   @override
   Widget build(BuildContext context) {
-    // final dummyList = List.generate(50, (index) => CatalogModel.item[0]);
-    // final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
-
     return Scaffold(
-      /// Scaffold has a body head and footer
-      appBar: AppBar(
-        // backgroundColor: Colors.white,
-        // elevation: 0.0,
-        // iconTheme: IconThemeData(color: Colors.black),
-        title: Text(
-          "Catalog App",
-          // style: TextStyle(color: Colors.black),
+      backgroundColor: Mytheme.creamColor,
+      body: SafeArea(
+        child: Container(
+          padding: Vx.m32,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CatalogHeader(),
+              if (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+                CatalogList().expand()
+              else
+                CircularProgressIndicator().centered().py16().expand(),
+            ],
+          ),
         ),
       ),
-      // Widgets are immutable means they are not changeable   blueprint  not paint only configuration     create element
-      // elememnt is mutable
-      // 3 widget trees one is on which we are working
-      // widget tree-
-      // to
-      // element tree-
-      // to
-      // render tree -
-      // canvass paper draw
-      // body:
-      // Container(
-      //   width: 100,
-      //   height: 100,
-      //   color: Colors.amberAccent,
-      // ),
-
-      // Container(
-      //   constraints: BoxConstraints(
-      //     maxHeight: 200,
-      //     maxWidth: 200,
-      //     minHeight: 70,
-      //     minWidth: 70,
-      //   ),
-      //   color: Colors.green,
-      //   child: Container(
-      //     height: 70,
-      //     width: 120,
-      //     color: Colors.amber,
-      //   ),
-      // ),
-      body: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
-            ? GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 16,
-                  crossAxisSpacing: 16,
-                ),
-
-                itemCount: CatalogModel.items!.length, // fix the length
-                itemBuilder: (BuildContext context, int index) {
-                  final item = CatalogModel.items![index];
-                  return Card(
-                    clipBehavior: Clip.antiAlias,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    child: GridTile(
-                        header: Container(
-                          child: Text(
-                            item.name,
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          padding: const EdgeInsets.all(12),
-                          decoration: BoxDecoration(color: Colors.deepPurple),
-                        ),
-                        child: Image.network(item.image),
-                        footer: Container(
-                          child: Text(
-                            item.price.toString(),
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          padding: const EdgeInsets.all(12),
-                          decoration: BoxDecoration(color: Colors.black),
-                        )),
-                  );
-                },
-              )
-
-            // ListView.builder(
-            //     itemCount: CatalogModel.items!.length,
-            //     itemBuilder: (context, index) {
-            //       return ItemWidget(
-            //         item: CatalogModel.items![index],
-            //       );
-            //     },
-            //   )
-            : const Center(
-                child: CircularProgressIndicator(),
-              ),
-      ),
-      // Center(
-      //   child: Container(
-      //     child: Text(
-      //       // context.runtimeType.toString(),
-      //       'Welcome Man you are going to complete flutter before $days March Best of luck $name',
-      //     ),
-      //   ),
-      // ),
-      drawer: MyDrawer(),
     );
   }
 }
+
+
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   // final dummyList = List.generate(50, (index) => CatalogModel.item[0]);
+  //   // final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
+
+  //   return Scaffold(
+      // /// Scaffold has a body head and footer
+      // appBar: AppBar(
+      //   // backgroundColor: Colors.white,
+      //   // elevation: 0.0,
+      //   // iconTheme: IconThemeData(color: Colors.black),
+      //   title: Text(
+      //     "Catalog App",
+      //     // style: TextStyle(color: Colors.black),
+      //   ),
+      // ),
+      // // Widgets are immutable means they are not changeable   blueprint  not paint only configuration     create element
+      // // elememnt is mutable
+      // // 3 widget trees one is on which we are working
+      // // widget tree-
+      // // to
+      // // element tree-
+      // // to
+      // // render tree -
+      // // canvass paper draw
+      // // body:
+      // // Container(
+      // //   width: 100,
+      // //   height: 100,
+      // //   color: Colors.amberAccent,
+      // // ),
+
+      // // Container(
+      // //   constraints: BoxConstraints(
+      // //     maxHeight: 200,
+      // //     maxWidth: 200,
+      // //     minHeight: 70,
+      // //     minWidth: 70,
+      // //   ),
+      // //   color: Colors.green,
+      // //   child: Container(
+      // //     height: 70,
+      // //     width: 120,
+      // //     color: Colors.amber,
+      // //   ),
+      // // ),
+      // body: Padding(
+      //   padding: const EdgeInsets.all(18.0),
+      //   child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+      //       ? GridView.builder(
+      //           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      //             crossAxisCount: 2,
+      //             mainAxisSpacing: 16,
+      //             crossAxisSpacing: 16,
+      //           ),
+
+      //           itemCount: CatalogModel.items!.length, // fix the length
+      //           itemBuilder: (BuildContext context, int index) {
+      //             final item = CatalogModel.items![index];
+      //             return Card(
+      //               clipBehavior: Clip.antiAlias,
+      //               shape: RoundedRectangleBorder(
+      //                   borderRadius: BorderRadius.circular(10)),
+      //               child: GridTile(
+      //                   header: Container(
+      //                     child: Text(
+      //                       item.name,
+      //                       style: TextStyle(color: Colors.white),
+      //                     ),
+      //                     padding: const EdgeInsets.all(12),
+      //                     decoration: BoxDecoration(color: Colors.deepPurple),
+      //                   ),
+      //                   child: Image.network(
+      //                     item.image,
+      //                     // height: 60,//// no change because sizes go down
+      //                     // fit: BoxFit.contain,// no change because sizes go down
+      //                   ),
+      //                   footer: Container(
+      //                     child: Text(
+      //                       item.price.toString(),
+      //                       style: TextStyle(color: Colors.white),
+      //                     ),
+      //                     padding: const EdgeInsets.all(12),
+      //                     decoration: BoxDecoration(color: Colors.black),
+      //                   )),
+      //             );
+      //           },
+      //         )
+
+      //       // ListView.builder(
+      //       //     itemCount: CatalogModel.items!.length,
+      //       //     itemBuilder: (context, index) {
+      //       //       return ItemWidget(
+      //       //         item: CatalogModel.items![index],
+      //       //       );
+      //       //     },
+      //       //   )
+      //       : const Center(
+      //           child: CircularProgressIndicator(),
+      //         ),
+      // ),
+      // // Center(
+      // //   child: Container(
+      // //     child: Text(
+      // //       // context.runtimeType.toString(),
+      // //       'Welcome Man you are going to complete flutter before $days March Best of luck $name',
+      // //     ),
+      // //   ),
+      // // ),
+      // drawer: MyDrawer(),
+//     );
+//   }
+// }
---
+++ lib/widgets/home_widgets/Catalog_list.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widgets/home_widgets/Catalog_Images.dart';
+
+
+import '../../models/catalog.dart';
+import '../../widgets/themes.dart';
+
+import 'package:velocity_x/velocity_x.dart';
+class CatalogList extends StatelessWidget {
+  // const CatalogList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: CatalogModel.items!.length,
+      itemBuilder: (context, index) {
+        final Item catalog = CatalogModel.items![index];
+        return CatalogItem(catalog: catalog);
+      },
+    );
+  }
+}
+
+class CatalogItem extends StatelessWidget {
+  final Item catalog;
+  const CatalogItem({
+    Key? key,
+    required this.catalog,
+  })  : assert(catalog != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+      child: Row(
+        children: [
+          CatalogImage(
+            image: catalog.image,
+          ),
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              catalog.name.text.lg.color(Mytheme.darkBluishColor).bold.make(),
+              catalog.desc.text.make(),
+              10.heightBox,
+              ButtonBar(
+                alignment: MainAxisAlignment.spaceBetween,
+                buttonPadding: EdgeInsets.zero,
+                children: [
+                  "\$${catalog.price}".text.bold.xl.make(),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Mytheme.darkBluishColor),
+                      shape: MaterialStateProperty.all(
+                        StadiumBorder(),
+                      ),
+                    ),
+                    child: "Buy".text.make(),
+                  ),
+                ],
+              ).pOnly(right: 8.0)
+            ],
+          ))
+        ],
+      ),
+    ).white.rounded.square(150).make().py16();
+  }
+}
